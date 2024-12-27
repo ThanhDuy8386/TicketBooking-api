95407f65c47d0c7a4d89105ab16f3989c33dbe73
eiuThanhDuy <duynguyen120504@gmail.com>
2024-12-27 22:26:38 +0700

    .
    .

---
+++ TicketBooking.Repositories/Repositories/BaseRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketBooking.Repositories.IRepositories;
+
+namespace TicketBooking.Repositories.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        public void Create(T entity)
+        {
+            
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(string id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
